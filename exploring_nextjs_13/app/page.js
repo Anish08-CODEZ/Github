595a4e69016995ae20560d6c30f2b278f6890b5d
Anish Programmer <anishtdubey@gmail.com>
2023-02-26 10:10:31 +0530

    Explored FONT OPTIMISATION in NextJS-13
    and removed some unwanted css from global.css

--- exploring_nextjs_13/app/page.js
+++ exploring_nextjs_13/app/page.js
@@ -1,19 +1,35 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Image from 'next/image';
+import { Inter, Poppins } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
+// Added a new font POPPINS from Google Font
+const poppins = Poppins({
+  weight: '500',
+  subsets: ['latin'],
+  style: ['normal']
+});
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h2>Exploring NextJS 13! 🚀</h2>
-      <Image 
-        src = "https://images.unsplash.com/photo-1504639725590-34d0984388bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
-        width = {500}
-        height = {500}
-        className='mt-1'
-      />
+    <main className={inter.className}>
+      <div className='p-6 w-full min-w-full flex items-center justify-center flex-col'>
+        <h2>Exploring NextJS 13! 🚀</h2>
+
+        {/* We explored Image optimisation in these lines */}
+        <Image 
+          src = "https://images.unsplash.com/photo-1504639725590-34d0984388bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+          width = {500}
+          height = {500}
+          className='mt-10 rounded-lg drop-shadow-md transition-all hover:drop-shadow-xl hover:scale-110'
+        />
+
+        {/* We are exploring FONT OPTIMISATION in these lines */}
+        <div className="mt-5">
+          <h2 className={poppins.className}>A new font: POPPINS</h2>
+          {/* <h2 className={sourceSans.className}>A new font</h2>
+          <h2 className={shantellSans.className}>A new font</h2> */}
+        </div>
+      </div>
     </main>
   )
 }
--- exploring_nextjs_13/next.config.js
+++ exploring_nextjs_13/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   experimental: {
     appDir: true,
+    fontLoaders: [
+      { loader: 'next/font/google', options: { weight: ['400', '500', '600', '700'], subsets: ['latin'] } }
+    ]
   },
   images: {
     remotePatterns: [
